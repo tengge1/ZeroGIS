07659e54edbf79ce1051a511d4d592a33e4f6964
liteng <930372551@qq.com>
2017-12-04 07:51:06 +0800

    change structure

---
+++ ZeroGIS/js/Object3D/Scene.js
@@ -0,0 +1,10 @@
+﻿/**
+* Scene
+*/
+WME.Object3D.Scene = function (args) {
+    WME.Object3D.Object3DComponents.apply(this, arguments);
+};
+
+WME.Object3D.Scene.prototype = new Object3DComponents();
+
+WME.Object3D.Scene.prototype.constructor = WME.Object3D.Scene;
