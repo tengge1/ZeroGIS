6c602bde186eb24f130404f0561fa11bd2a75625
liteng <930372551@qq.com>
2017-12-06 08:03:44 +0800

    修改架构

---
+++ ZeroGIS/js/Kernel/TileGrid.js
@@ -0,0 +1,52 @@
+﻿ZeroGIS.TileGrid = function (level, row, column) {
+    if (!Utils.isNonNegativeInteger(level)) {
+        throw "invalid level";
+    }
+    if (!Utils.isNonNegativeInteger(row)) {
+        throw "invalid row";
+    }
+    if (!Utils.isNonNegativeInteger(column)) {
+        throw "invalid column";
+    }
+    this.level = level;
+    this.row = row;
+    this.column = column;
+};
+
+ZeroGIS.TileGrid.prototype._requireMath = function () {
+    return require("world/Math");
+};
+
+ZeroGIS.TileGrid.prototype.equals = function (other) {
+    return other instanceof ZeroGIS.TileGrid && this.level == other.level && this.row == other.row && this.column == other.column;
+};
+
+ZeroGIS.TileGrid.prototype.getLeft = function () {
+    var MathUtils = this._requireMath();
+    return MathUtils.getTileGridByBrother(this.level, this.row, this.column, MathUtils.LEFT);
+};
+
+ZeroGIS.TileGrid.prototype.getRight = function () {
+    var MathUtils = this._requireMath();
+    return MathUtils.getTileGridByBrother(this.level, this.row, this.column, MathUtils.RIGHT);
+};
+
+ZeroGIS.TileGrid.prototype.getTop = function () {
+    var MathUtils = this._requireMath();
+    return MathUtils.getTileGridByBrother(this.level, this.row, this.column, MathUtils.TOP);
+};
+
+ZeroGIS.TileGrid.prototype.getBottom = function () {
+    var MathUtils = this._requireMath();
+    return MathUtils.getTileGridByBrother(this.level, this.row, this.column, MathUtils.BOTTOM);
+};
+
+ZeroGIS.TileGrid.prototype.getParent = function () {
+    var MathUtils = this._requireMath();
+    return MathUtils.getTileGridAncestor(this.level - 1, this.level, this.row, this.column);
+};
+
+ZeroGIS.TileGrid.prototype.getAncestor = function (ancestorLevel) {
+    var MathUtils = this._requireMath();
+    return MathUtils.getTileGridAncestor(ancestorLevel, this.level, this.row, this.column);
+};
---
+++ ZeroGIS/js/TiledLayer/TiandituTiledLayer.js
@@ -0,0 +1,19 @@
+﻿/**
+* TiandituTiledLayer
+*/
+ZeroGIS.TiledLayer.TiandituTiledLayer = function (args) {
+    ZeroGIS.TiledLayer.apply(this, arguments);
+};
+
+ZeroGIS.TiledLayer.TiandituTiledLayer.prototype = new ZeroGIS.TiledLayer();
+
+ZeroGIS.TiledLayer.TiandituTiledLayer.prototype.constructor = ZeroGIS.TiledLayer.TiandituTiledLayer;
+
+ZeroGIS.TiledLayer.TiandituTiledLayer.prototype.getImageUrl = function (level, row, column) {
+    ZeroGIS.TiledLayer.prototype.getImageUrl.apply(this, arguments);
+    var url = "";
+    var sum = level + row + column;
+    var serverIdx = sum % 8;
+    url = "//t" + serverIdx + ".tianditu.com/DataServer?T=vec_w&x=" + column + "&y=" + row + "&l=" + level;
+    return url;
+};
